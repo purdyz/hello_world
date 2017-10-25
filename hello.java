6b320d77a409149ef3a6e6e48832022bb2a1189e
Zack <zacharypurdy@gmail.com>
2017-10-25 10:11:11 -0400

    Added user input code

--- hello.java
+++ hello.java
@@ -1,15 +1,16 @@
 // a simple program to practice common Github commands 
+import java.util.Scanner;
 
 public class hello {
 
     public static void main(String [] args) {
-
-
+		System.out.println("Enter your name: ");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
 	
-	// added  line	
-	// added another line
+	
 
-        System.out.println("Hello Class!");
+        System.out.println("Hello " + name + ", my name is Zack!");
 
 
 
